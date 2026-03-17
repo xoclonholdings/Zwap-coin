bec24d9ed483b6b3610d3450b144df39ff58871f
XOCLON HOLDINGS INC. <devin@xoclonholdings.property>
2026-03-17 07:13:25 -0400

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -5,7 +5,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 import os
 import logging
 from pathlib import Path
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel
 from typing import List, Optional
 import uuid
 from datetime import datetime, timezone, timedelta
