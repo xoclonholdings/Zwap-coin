3e6070c5ca5e452519c6aac3a6ad357f63178a02
XOCLON HOLDINGS INC. <devin@xoclonholdings.property>
2026-03-21 07:00:55 -0400

    Update __init__.py

--- backend/routers/admin/__init__.py
+++ backend/routers/admin/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 
 from .dashboard import router as dashboard_router
 from .users import router as users_router
+from .marketplace import router as marketplace_router
 
 admin_router = APIRouter(prefix="/admin", tags=["Admin"])
 admin_router.include_router(dashboard_router)
 admin_router.include_router(users_router)
+admin_router.include_router(marketplace_router)
 
 router = admin_router
