4c744723417f1dadaf4947c224986d93dd3352b4
XOCLON HOLDINGS INC. <devin@xoclonholdings.property>
2026-04-03 02:28:03 -0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/index.css";
 import App from "@/App";
+import PrivyProviderWrapper from "./app/PrivyProviderWrapper";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <App />
+  <React.StrictMode>
+    <PrivyProviderWrapper>
+      <App />
+    </PrivyProviderWrapper>
+  </React.StrictMode>
 );
