5d56badb29a209236c9b882d4ca4a081faa140a8
XOCLON HOLDINGS INC. <devin@xoclonholdings.property>
2026-04-06 10:47:50 -0400

    Update DeveloperSubmissionPage.jsx

--- frontend/src/components/ui/dashboard/drawer/DeveloperSubmissionPage.jsx
+++ frontend/src/components/ui/dashboard/drawer/DeveloperSubmissionPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { ArrowLeft } from "lucide-react";
-import PlaySubmissionPortal from "../play/PlaySubmissionPortal";
+import PlaySubmissionPortal from "./PlaySubmissionPortal";
 
 export default function DeveloperSubmissionPage() {
   const navigate = useNavigate();
