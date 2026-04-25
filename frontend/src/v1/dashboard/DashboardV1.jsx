171c238133f69ec2e21ccfb4ac2e41a411f57697
XOCLON HOLDINGS INC. <214429976+xoclonholdings@users.noreply.github.com>
2026-04-25 15:32:10 -0400

    Update DashboardV1.jsx

--- frontend/src/v1/dashboard/DashboardV1.jsx
+++ frontend/src/v1/dashboard/DashboardV1.jsx
@@ -1,98 +1,43 @@
 import React from "react";
 import useV1DashboardState from "./useV1DashboardState";
+import AppHeaderV1 from "./AppHeaderV1";
 import DashboardWindowMove from "./windows/DashboardWindowMove";
 import DashboardWindowPlay from "./windows/DashboardWindowPlay";
 import DashboardWindowShop from "./windows/DashboardWindowShop";
 import DashboardWindowZwap from "./windows/DashboardWindowZwap";
 
-export default function DashboardV1({
-  user,
-  authUser,
-  todaySteps,
-  stepGoal,
-  isMoveActive,
-  gamesPlayedToday,
-  playGoal,
-  isPlayActive,
-  completedTasks,
-  totalTasks,
-  taskStates,
-  zptsBalance,
-  shopUnlocked,
-  systemMode,
-  systemMessage,
-  systemEventType,
-  systemNextStep,
-  idleMessages,
-  learnUnlocked = false,
-  streamUnlocked = false,
-  swapUnlocked = false,
-  onOpenMove,
-  onOpenPlay,
-  onOpenTasks,
-  onOpenZwap,
-}) {
+export default function DashboardV1() {
   const {
-    move,
-    play,
-    zpts,
-    unlocks,
-    zwap,
-  } = useV1DashboardState({
-    user,
-    authUser,
-    todaySteps,
-    stepGoal,
-    isMoveActive,
+    steps,
+    stepsPercent,
     gamesPlayedToday,
-    playGoal,
-    isPlayActive,
-    completedTaskCount: completedTasks,
-    totalTaskCount: totalTasks,
-    taskStates,
+    playPercent,
     zptsBalance,
-    shopUnlocked,
-    zwapMode: systemMode,
-    zwapMessage: systemMessage,
-    zwapHint: systemNextStep,
-  });
+    zptsPercent,
+  } = useV1DashboardState();
 
   return (
     <div className="w-full flex justify-center px-3 pb-6">
       <div className="w-full max-w-[430px] flex flex-col gap-3">
+        <AppHeaderV1 />
+
         <DashboardWindowMove
-          todaySteps={move.todaySteps}
-          stepGoal={move.stepGoal}
-          isActive={move.isMoveActive}
-          progressPercent={move.moveProgressPercent}
-          onOpenMove={onOpenMove}
+          steps={steps}
+          stepsPercent={stepsPercent}
         />
 
         <DashboardWindowPlay
-          gamesPlayedToday={play.gamesPlayedToday}
-          playGoal={play.playGoal}
-          isActive={play.isPlayActive}
-          progressPercent={play.playProgressPercent}
-          onOpenPlay={onOpenPlay}
+          gamesPlayedToday={gamesPlayedToday}
+          playPercent={playPercent}
         />
 
         <DashboardWindowShop
-          zptsBalance={zpts.zptsBalance}
-          onOpenTasks={onOpenTasks}
-          shopUnlocked={unlocks.shopUnlocked}
+          zptsBalance={zptsBalance}
         />
 
         <DashboardWindowZwap
-          mode={zwap.zwapMode}
-          systemMessage={zwap.zwapMessage}
-          eventType={systemEventType}
-          nextStep={zwap.zwapHint}
-          idleMessages={idleMessages}
-          shopUnlocked={unlocks.shopUnlocked}
-          learnUnlocked={learnUnlocked}
-          streamUnlocked={streamUnlocked}
-          swapUnlocked={swapUnlocked}
-          onOpenZwap={onOpenZwap}
+          zptsBalance={zptsBalance}
+          zptsPercent={zptsPercent}
         />
       </div>
     </div>
