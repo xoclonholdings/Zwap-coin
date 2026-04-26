9452975b2c4a38068fbd4a016a6e5aaf6ebec043
xoclonholdings <xoclon.holdings@gmail.com>
2026-04-26 09:53:29 -0400

    Cleaned V2 files, aligned repo to V1 structure

--- frontend/src/components/user/AccountRail.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import AccountPanelContent from "./AccountPanelContent";
-
-export default function AccountRail() {
-  return <AccountPanelContent showHeader={false} />;
-}
