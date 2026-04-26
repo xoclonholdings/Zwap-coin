ce2906a8c15df7250c9edc44638498c8329364fb
XOCLON HOLDINGS INC. <214429976+xoclonholdings@users.noreply.github.com>
2026-04-26 05:37:18 -0400

    Create ZapManConstants.js

---
+++ frontend/src/v1/components/games/zapman/ZapManConstants.js
@@ -0,0 +1,13 @@
+export const GRID_SIZE = 15;
+
+export const INITIAL_PLAYER_SPEED = 1;
+export const INITIAL_ENEMY_SPEED = 0.6;
+
+export const MAX_ENEMIES = 6;
+
+export const PELLET_COUNT = 40;
+
+export const ROUND_SCALING = {
+  enemySpeedIncrement: 0.1,
+  maxEnemiesIncrement: 1,
+};
