df1769b09d60abec96ad6e6fe752e6accc74ef62
XOCLON HOLDINGS INC. <214429976+xoclonholdings@users.noreply.github.com>
2026-04-28 17:15:51 -0400

    Update AppContent.jsx

--- frontend/src/app/AppContent.jsx
+++ frontend/src/app/AppContent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 import V1App from "@/v1/V1App";
-import LearnPage from "@/v1/learn/LearnPage";
+import LearnPage from "@/v1/dashboard/windows/learn/LearnPage";
 import AdminPanelV1 from "@/v1/dashboard/admin/AdminPanelV1";
 
 export default function AppContent() {
