123c48b1184e9d47b34d3d5fb10bcd71e1446525
XOCLON HOLDINGS INC. <214429976+xoclonholdings@users.noreply.github.com>
2026-05-02 20:18:04 -0400

    Update StreamPlayerSurface.jsx

--- frontend/src/v1/dashboard/windows/stream/StreamPlayerSurface.jsx
+++ frontend/src/v1/dashboard/windows/stream/StreamPlayerSurface.jsx
@@ -38,26 +38,24 @@ export default function StreamPlayerSurface({ item, activeTab }) {
           </div>
         </div>
 
-        <div className="mt-3">
+        <div className="mt-4 flex justify-center">
           {isRadio && (
-            <div className="overflow-hidden rounded-[16px] bg-black">
-              <iframe
-                style={{ border: 0, width: "100%", height: "208px" }}
-                src="https://bandcamp.com/EmbeddedPlayer/album=2559372961/size=large/bgcol=333333/linkcol=0f91ff/artwork=small/tracklist=false/transparent=true/"
-                seamless
-                title="ZWAP Radio"
-                allow="autoplay; encrypted-media"
-              >
-                <a href="https://poetreesmusic.bandcamp.com/album/zwap-radio">
-                  ZWAP! Radio by ZWAP!
-                </a>
-              </iframe>
-            </div>
+            <iframe
+              style={{ border: 0, width: "210px", height: "210px" }}
+              src="https://bandcamp.com/EmbeddedPlayer/album=2559372961/size=large/bgcol=333333/linkcol=0f91ff/artwork=small/minimal=true/transparent=true/"
+              seamless
+              title="ZWAP Radio"
+              allow="autoplay; encrypted-media"
+            >
+              <a href="https://poetreesmusic.bandcamp.com/album/zwap-radio">
+                ZWAP! Radio by ZWAP!
+              </a>
+            </iframe>
           )}
 
           {isSpotify && (
-            <div className="space-y-3">
-              <div className="flex h-[92px] items-center justify-center rounded-[16px] border border-white/10 bg-black/20">
+            <div className="space-y-4 w-full">
+              <div className="flex h-[100px] items-center justify-center rounded-[16px] border border-white/10 bg-black/20">
                 <Headphones className="h-6 w-6 text-green-400" />
               </div>
 
@@ -72,7 +70,7 @@ export default function StreamPlayerSurface({ item, activeTab }) {
           )}
 
           {isApple && (
-            <div className="flex h-[92px] items-center justify-center rounded-[16px] border border-white/10 bg-black/20 text-center">
+            <div className="flex h-[100px] w-full items-center justify-center rounded-[16px] border border-white/10 bg-black/20 text-center">
               <div>
                 <Music2 className="mx-auto mb-2 h-6 w-6 text-pink-300" />
                 <p className="text-sm text-white/80">
