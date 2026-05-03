082999a318ab5162b8f291528f249c2dbab37bcc
XOCLON HOLDINGS INC. <214429976+xoclonholdings@users.noreply.github.com>
2026-05-03 17:02:53 -0400

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/app/",
+  base: "./", // 🔥 THIS FIXES YOUR ISSUE
   plugins: [react()],
   resolve: {
     alias: {
