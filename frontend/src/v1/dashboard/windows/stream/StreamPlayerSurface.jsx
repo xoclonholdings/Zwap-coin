75bae5cc1d21efb0060bd0ea30a29dcfd9e0fcdc
XOCLON HOLDINGS INC. <214429976+xoclonholdings@users.noreply.github.com>
2026-05-04 13:13:51 -0400

    Update StreamPlayerSurface.jsx

--- frontend/src/v1/dashboard/windows/stream/StreamPlayerSurface.jsx
+++ frontend/src/v1/dashboard/windows/stream/StreamPlayerSurface.jsx
@@ -41,7 +41,7 @@ export default function StreamPlayerSurface({ item, activeTab }) {
         <div className="mt-4 flex justify-center">
           {isRadio && (
             <iframe
-              style={{ border: 0, width: "260px", height: "260px" }}
+              style={{ border: 0, width: "250px", height: "250px" }}
               src="https://bandcamp.com/EmbeddedPlayer/album=2559372961/size=large/bgcol=333333/linkcol=0f91ff/minimal=true/transparent=true/"
               seamless
               title="ZWAP Radio"
